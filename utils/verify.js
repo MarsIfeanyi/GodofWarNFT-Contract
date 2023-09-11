1900077d39eccfeca7ee1d22e03a1f9e6c9f3113
MarsIfeanyi <marschalice@gmail.com>
2023-09-11 19:45:52 +0100

    Initial Commit

---
+++ utils/verify.js
@@ -0,0 +1,21 @@
+const { run } = require("hardhat");
+
+const contractAddress = "0xc64858Ca415E62dA10166b4A1a22481A00C2A251";
+
+const verify = async (contractAddress, args) => {
+  console.log("Verifying contract...");
+  try {
+    await run("verify:verify", {
+      address: contractAddress,
+      constructorArguments: args,
+    });
+  } catch (e) {
+    if (e.message.toLowerCase().includes("already verified")) {
+      console.log("Already verified!");
+    } else {
+      console.log(e);
+    }
+  }
+};
+
+module.exports = { verify };
